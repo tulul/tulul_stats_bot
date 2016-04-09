2e8d06e2592e357cd378e1124c5d181e2c994596
Rick Daniel <rick@araishikeiwai.com>
2016-04-09 13:21:04 +0100

    fixed rank and added percentage

--- lib/tulul_stats_bot/tulul_stats/group.rb
+++ lib/tulul_stats_bot/tulul_stats/group.rb
@@ -17,11 +17,33 @@ module TululStats
     end
 
     def top(field)
-      count = 0
-      self.users.sort_by{ |b| eval("b.#{field}") }.reverse.map do |user|
+      res = self.users.sort_by{ |b| eval("b.#{field}") }.reverse.map do |user|
         sum = eval("user.#{field}")
-        "#{count += 1}. #{user.full_name}: #{sum}" if sum > 0
-      end.compact.join("\n")
+        [name, sum] if sum > 0
+      end.compact
+
+      total = res.inject(0){ |b, c| b + c[1] }.to_f
+
+      rank = 0
+      prev_sum = -1
+      prev_count = 1
+
+      res.each do |entry|
+        name = entry[0]
+        sum = entry[1]
+
+        if sum == prev_sum
+          prev_count += 1
+        else
+          rank += prev_count
+          prev_count = 1
+          prev_sum = sum
+        end
+
+        percentage = "%.2f" % (sum * 100 / total) rescue 0
+
+        "#{rank}. #{name}: #{sum} (#{percentage}%)"
+      end.join("\n")
     end
   end
 end
